220b52a3448844731d24ba69cdfd966ec860d4a2
Tushar1082 <134959329+Tushar1082@users.noreply.github.com>
2025-02-19 23:03:10 +0530

    Update slider.jsx

--- E-commerce/FrontEnd/src/components/homepage/slider/slider.jsx
+++ E-commerce/FrontEnd/src/components/homepage/slider/slider.jsx
@@ -1,74 +1,98 @@
-import React, { useState, useEffect } from "react";
-import CryptoJS from "crypto-js";
+import React,{useEffect} from 'react'; 
+import CryptoJS from 'crypto-js';
 import "./slider.css";
 
 export default function SliderPhone() {
-  const encryptSale50 = CryptoJS.AES.encrypt(
-    "sale=50",
-    import.meta.env.VITE_REACT_EncryptKey
-  );
-  const encryptSale40 = CryptoJS.AES.encrypt(
-    "sale=40",
-    import.meta.env.VITE_REACT_EncryptKey
-  );
+  const encryptSale50 = CryptoJS.AES.encrypt("sale=50",import.meta.env.VITE_REACT_EncryptKey);
+  const encryptSale40 = CryptoJS.AES.encrypt("sale=40",import.meta.env.VITE_REACT_EncryptKey);
 
   const imgArr = [
     {
-      img: "images/slider/asus.jpg",
-      link: `/buyPage?category=laptops&&${encryptSale50.toString()}&&id=16`,
+      img:"images/slider/asus.jpg",
+      link:`/buyPage?category=laptops&&${encryptSale50.toString()}&&id=16`
     },
-    { img: "images/slider/womenSale.png", link: "/category/women" },
-    { img: "images/slider/menSale.png", link: "/category/men" },
     {
-      img: "images/slider/mobileSale.png",
-      link: `/buyPage?category=realme&&${encryptSale40.toString()}&&id=6`,
+     img:"images/slider/womenSale.png",
+     link:"/category/women"
     },
-    { img: "images/slider/electronicSale.jpg", link: "/category/electronics" },
     {
-      img: "images/slider/ps5.jpg",
-      link: `/buyPage?category=gamingConsoles&&${encryptSale50.toString()}&&id=11`,
+     img:"images/slider/menSale.png",
+     link:"/category/men"
     },
-    { img: "images/slider/laptopSale.png", link: "/category/electronics/laptops" },
-  ];
+    {
+      img:"images/slider/mobileSale.png",
+      link:`/buyPage?category=realme&&${encryptSale40.toString()}&&id=6`
+    },
+    {
+      img:"images/slider/electronicSale.jpg",
+      link:"/category/electronics"
+    },
+    {
+      img:"images/slider/ps5.jpg",
+      link:`/buyPage?category=gamingConsoles&&${encryptSale50.toString()}&&id=11`
+    },
+    {
+      img:"images/slider/laptopSale.png",
+      link:"/category/electronics/laptops"
+    }];
+  
+  const slides = document.getElementsByClassName("slides");
+  let count=0;
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  function next() {
-    setCurrentIndex((prev) => (prev + 1) % imgArr.length);
+  function slideImg(){
+    for(let i=0;i<slides.length;i++){
+      slides[i].style.transform = `translateX(-${count*100}%)`;
+    }
   }
-
-  function prev() {
-    setCurrentIndex((prev) => (prev - 1 + imgArr.length) % imgArr.length);
+  function next(){
+    if(count<slides.length-1){
+      count++;
+      slideImg();
+    }else if(count===slides.length-1){
+      while(count>=0){
+        count--;
+        slideImg();
+      }
+    }
   }
-
-  useEffect(() => {
-    const interval = setInterval(next, 4000);
-    return () => clearInterval(interval); // Clears the interval when unmounted
-  }, []);
-
+  function prev(){
+    if(count>0){
+      count--;
+      slideImg();
+    }
+  }
+  function autoSide(){
+    setInterval(()=>{
+      next()
+    },4000)
+  }
+  useEffect(()=>{
+    for(let i=0;i<slides.length;i++){
+        slides[i].style.left= `${i*100}%`;
+      }
+      autoSide();
+  },[])
+  
   return (
-    <div style={{ backgroundColor: "#d3d3d34a" }}>
-      <div id="mainSlider">
-        {imgArr.map((elem, index) => (
-          <img
-            key={index}
-            src={elem.img}
-            alt="slider"
-            className="slides"
-            onClick={() => (window.location.href = elem.link)}
-           
-            style={{
-              cursor: "pointer",
-              transform: `translateX(-${currentIndex * 100}%)`,
-              transition: "transform 1s ease-in-out",
-            }}
-          />
-        ))}
-        <div id="sliderBtn">
-          <button onClick={prev} className="btn1SP">&lt;</button>
-          <button onClick={next} className="btn2SP">&gt;</button>
-        </div>
+    <div style={{backgroundColor:'#d3d3d34a'}}>
+      <div id='mainSlider'>
+          {imgArr.map((elem,index)=>(
+            <>
+              <img src={elem.img}
+                alt="error" 
+                key={index} 
+                className='slides'
+                onClick={()=>window.location.href=elem.link}
+                style={{cursor:"pointer"}}
+                loading="lazy"
+                />
+            </>
+          ))
+          }
+          <div id='sliderBtn'>
+            <button onClick={prev} className='btn1SP' id='btnRef' >&lt;</button>
+            <button onClick={next} className='btn2SP' id='btnRef' >&gt;</button>
+          </div>
       </div>
     </div>
-  );
-}
+    )};
