5c97af7d0912b5aa83ea98cd53c5e844d837ba8c
Tushar1082 <134959329+Tushar1082@users.noreply.github.com>
2025-02-20 12:05:10 +0530

    Update events.jsx

--- E-commerce/FrontEnd/src/components/homepage/events/events.jsx
+++ E-commerce/FrontEnd/src/components/homepage/events/events.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useState,useRef } from 'react';
 import "./events.css";
 import { Link } from 'react-router-dom';
-import lehengaImg from '/images/events/7.jpg';
-import lehengaOff from '/images/events/lehengaOff.png';
-import tShirtImg from '/images/events/cloth.jpg';
-import tShirtOff from '/images/events/t-shirtOff.png';
-import sareeImg from '/images/events/4.jpg';
-import sareeOff from '/images/events/sareeOff.png';
-import blazerImg from '/images/events/5.jpg';
-import blazerOff from '/images/events/blazarOff.png';
-import bookImg from '/images/events/6.jpg';
-import bookOff from '/images/events/bookOff.png';
+// import lehengaImg from '/images/events/7.jpg';
+// import lehengaOff from '/images/events/lehengaOff.png';
+// import tShirtImg from '/images/events/cloth.jpg';
+// import tShirtOff from '/images/events/t-shirtOff.png';
+// import sareeImg from '/images/events/4.jpg';
+// import sareeOff from '/images/events/sareeOff.png';
+// import blazerImg from '/images/events/5.jpg';
+// import blazerOff from '/images/events/blazarOff.png';
+// import bookImg from '/images/events/6.jpg';
+// import bookOff from '/images/events/bookOff.png';
+
+// images for mobile view
+import lehengaImgMobileView from '/images/events/mobileView/7.jpg';
+import lehengaOffMobileView from '/images/events/mobileView/lehengaOff.jpeg';
+import tShirtImgMobileView from '/images/events/mobileView/cloth.jpg';
+import tShirtOffMobileView from '/images/events/mobileView/t-shirtOff.jpeg';
+import sareeImgMobileView from '/images/events/mobileView/4.jpg';
+import sareeOffMobileView from '/images/events/mobileView/sareeOff.jpeg';
+import blazerImgMobileView from '/images/events/mobileView/5.jpg';
+import blazerOffMobileView from '/images/events/mobileView/blazarOff.jpeg';
+import bookImgMobileView from '/images/events/mobileView/6.jpg';
+import bookOffMobileView from '/images/events/mobileView/bookOff.jpeg';
 import CryptoJS from 'crypto-js';
 
 function OtherEvent() {
@@ -117,6 +129,84 @@ function OtherEvent() {
       </div>
     </div>
   );
+}
+function OtherEventPhone(){
+  const mainRef = useRef();
+  const divImgRef1 = useRef();
+  const divImgRef2 = useRef();
+  const divImgRef3 = useRef();
+  const divImgRef4 = useRef();
+  const divImgRef5 = useRef();
+
+  function isElementOnScreen(element) {
+    var rect = element.getBoundingClientRect();
+    var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+  
+    // Check if the element is within the viewport
+    return (
+      (rect.top <= windowHeight) &&
+      ((rect.top + rect.height) >= 0) &&
+      (rect.left <= windowWidth) &&
+      ((rect.left + rect.width) >= 0)
+    );
+}
+
+function eventsTemplateImg(){
+const element = mainRef.current;
+const divImg1 = divImgRef1.current;
+const divImg2 = divImgRef2.current;
+const divImg3 = divImgRef3.current;
+const divImg4 = divImgRef4.current;
+const divImg5 = divImgRef5.current;
+
+if(element){
+  window.addEventListener('scroll',()=>{
+    if(isElementOnScreen(element)){
+      divImg1.style.display = "block";
+      divImg2.style.display = "block";
+      divImg3.style.display = "block";
+      divImg4.style.display = "block";
+      divImg5.style.display = "block";
+
+    }else{
+      divImg1.style.display = "none";
+      divImg2.style.display = "none";
+      divImg3.style.display = "none";
+      divImg4.style.display = "none";
+      divImg5.style.display = "none";
+    }
+  })
+}
+}
+useEffect(()=>{
+  eventsTemplateImg();
+},[])
+
+  return(
+    <div id='mainOEP' ref={mainRef}>
+      <div id='lehengaOEP' ref={divImgRef1}>
+        <img src={lehengaImgMobileView} alt="Error" />
+        <img src={lehengaOffMobileView} alt="Error" />
+      </div>
+      <div id='tShirtOEP' ref={divImgRef2}>
+        <img src={tShirtImgMobileView} alt="error" />
+        <img src={tShirtOffMobileView} alt="error" />
+      </div>
+      <div id='sareeOEP' ref={divImgRef3}>
+        <img src={sareeImgMobileView} alt="error" />
+        <img src={sareeOffMobileView} alt="error" />
+      </div>
+      <div id='blazerOEP' ref={divImgRef4}>
+        <img src={blazerImgMobileView} alt="error" />
+        <img src={blazerOffMobileView} alt="error" />
+      </div>
+      <div id='bookOEP' ref={divImgRef5}>
+        <img src={bookImgMobileView} alt="error" />
+        <img src={bookOffMobileView} alt="error" />
+      </div>
+    </div>
+  )
 }
 
 
@@ -237,85 +327,126 @@ export default function Events() {
       </div>
     </div>
     <OtherEvent/>
-    <OtherEventPhone/>
     </>
   );
 }
-function OtherEventPhone(){
-  const mainRef = useRef();
-  const divImgRef1 = useRef();
-  const divImgRef2 = useRef();
-  const divImgRef3 = useRef();
-  const divImgRef4 = useRef();
-  const divImgRef5 = useRef();
+export function EventsPhone() {
+  const imagesMobileView=["/images/events/mobileView/1.jpg","/images/events/mobileView/2.jpeg","/images/events/mobileView/3.jpg","/images/events/mobileView/game.jpg"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isTransitioning, setTransitioning] = useState(false);
 
-  function isElementOnScreen(element) {
-    var rect = element.getBoundingClientRect();
-    var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-    var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
-  
-    // Check if the element is within the viewport
-    return (
-      (rect.top <= windowHeight) &&
-      ((rect.top + rect.height) >= 0) &&
-      (rect.left <= windowWidth) &&
-      ((rect.left + rect.width) >= 0)
-    );
-}
+  useEffect(() => {
+    const sliderInterval = setInterval(nextSlide, 3000); // Change slide every 3 seconds
 
-function eventsTemplateImg(){
-const element = mainRef.current;
-const divImg1 = divImgRef1.current;
-const divImg2 = divImgRef2.current;
-const divImg3 = divImgRef3.current;
-const divImg4 = divImgRef4.current;
-const divImg5 = divImgRef5.current;
+    return () => {
+      clearInterval(sliderInterval);
+    };
+  }, [currentIndex]);
 
-if(element){
-  window.addEventListener('scroll',()=>{
-    if(isElementOnScreen(element)){
-      divImg1.style.display = "block";
-      divImg2.style.display = "block";
-      divImg3.style.display = "block";
-      divImg4.style.display = "block";
-      divImg5.style.display = "block";
+  const nextSlide = () => {
+    setTransitioning(true);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % imagesMobileView.length);
+      setTransitioning(false);
+    }, 500); // Transition duration is 0.5 seconds
+  };
 
-    }else{
-      divImg1.style.display = "none";
-      divImg2.style.display = "none";
-      divImg3.style.display = "none";
-      divImg4.style.display = "none";
-      divImg5.style.display = "none";
+  // const prevSlide = () => {
+  //   setTransitioning(true);
+  //   setTimeout(() => {
+  //     setCurrentIndex((prevIndex) =>
+  //       (prevIndex - 1 + images.length) % images.length
+  //     );
+  //     setTransitioning(false);
+  //   }, 500); // Transition duration is 0.5 seconds
+  // };
+  function handleChange(index){
+    setTransitioning(true);
+    setTimeout(()=>{
+      setCurrentIndex(index);
+      setTransitioning(false);
+    },500)
+  }
+  function eventTemplate(currentIndex){
+    switch(currentIndex){
+      case 0:
+        window.location.href="/category/women/jackets";
+      break;
+      case 1:
+        window.location.href="/category/men/jackets";
+      break;
+      case 2:
+        window.location.href="/category/women/tops";
+      break;
+      case 3:
+        window.location.href=`/buyPage${navigateTo("gamingConsoles",50,2)}`;
+        break;
+      default:
+        window.location.href="/";
     }
-  })
-}
-}
-useEffect(()=>{
-  eventsTemplateImg();
-},[])
+  }
+  function navigateTo(category,sale,id){
+    const encSale = "sale="+sale.toString();
+    const encryS = CryptoJS.AES.encrypt(encSale,import.meta.env.VITE_REACT_EncryptKey);
+    return `?category=${category}&&${encryS.toString()}&&id=${id}`;
+  }
+  return (
+    <>
+    <div id='mainEventPhone'>
+      <div className={`sliderEvent ${isTransitioning ? 'transitioning' : ''}`} >
+        <img
+          src={imagesMobileView[currentIndex]}
+          alt={`Slide ${currentIndex + 1}`}
+          className="sliderImgEvent"
+          onClick={()=>eventTemplate(currentIndex)}
+        />
+        <div className='dotBtnEvent'>
+          {imagesMobileView.map((elem,index)=>(
+            <button key ={index} onClick={()=>handleChange(index)}></button>
+          ))}
+        </div>
+      </div>
+      <div className='otherEvent'>
+         <div id='otherEvent1'>
+            <div id='otherEventDiv1' >
+            <Link to={{pathname:"/buyPage",search:navigateTo("laptops",50,13)}}>
+              <div>
+                <h4>APPLE 2023 MacBook Pro M2 Max</h4>
+                <p>from ₹2,85,490 at 7%off</p>
+              </div>
+            </Link>
+            </div>
+            <div id='otherEventDiv2' >
+            <Link to={{pathname:"/buyPage",search:navigateTo("televisions",50,2)}}>
+              <div>
+                <h4>Kodak 106 cm (42 inches) Full HD</h4>
+                <p> from ₹15,999</p>
+              </div>
+            </Link>
+            </div>
+          </div>
 
-  return(
-    <div id='mainOEP' ref={mainRef}>
-      <div id='lehengaOEP' ref={divImgRef1}>
-        <img src={lehengaImg} alt="Error" />
-        <img src={lehengaOff} alt="Error" />
-      </div>
-      <div id='tShirtOEP' ref={divImgRef2}>
-        <img src={tShirtImg} alt="error" />
-        <img src={tShirtOff} alt="error" />
-      </div>
-      <div id='sareeOEP' ref={divImgRef3}>
-        <img src={sareeImg} alt="error" />
-        <img src={sareeOff} alt="error" />
-      </div>
-      <div id='blazerOEP' ref={divImgRef4}>
-        <img src={blazerImg} alt="error" />
-        <img src={blazerOff} alt="error" />
-      </div>
-      <div id='bookOEP' ref={divImgRef5}>
-        <img src={bookImg} alt="error" />
-        <img src={bookOff} alt="error" />
+          <div id='otherEvent2'>
+            <div id='otherEventDiv3' >
+            <Link to={{pathname:"/buyPage",search:navigateTo("speakers",50,9)}}>
+              <div>
+                  <h4>Alexa with Alexa Assistant Smart Speaker  (Black)</h4>
+                  <p> from ₹4,450</p>
+               </div>
+            </Link>
+            </div>
+            <div id='otherEventDiv4' >
+            <Link to={{pathname:"/buyPage",search:navigateTo("headphones",50,5)}}>
+              <div>
+                <h4>boAt Immortal 131 with Beast Mode, 40 Hours Playback</h4>
+                <p>from ₹13,999</p>
+              </div>
+            </Link>
+            </div>
+         </div> 
       </div>
     </div>
-  )
+    <OtherEventPhone/>
+    </>
+  );
 }
