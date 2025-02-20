4c5106cb9b761aeeab7048d871a4c1d8a1140d68
Tushar1082 <134959329+Tushar1082@users.noreply.github.com>
2025-02-20 10:24:07 +0530

    Update slider.jsx

--- E-commerce/FrontEnd/src/components/homepage/slider/slider.jsx
+++ E-commerce/FrontEnd/src/components/homepage/slider/slider.jsx
@@ -4,7 +4,25 @@ import "./slider.css";
 
 export default function SliderPhone() {
   const encryptSale50 = CryptoJS.AES.encrypt("sale=50",import.meta.env.VITE_REACT_EncryptKey);
-  const encryptSale40 = CryptoJS.AES.encrypt("sale=40",import.meta.env.VITE_REACT_EncryptKey);
+
+  const imgArr = [
+    {
+      img:"images/slider/asus.jpg",
+      link:`/buyPage?category=laptops&&${encryptSale50.toString()}&&id=16`
+    },
+    {
+     img:"images/slider/womenSale.png",
+     link:"/category/women"
+    },
+    {
+     img:"images/slider/menSale.png",
+     link:"/category/men"
+    },
+    {
+      img:"images/slider/ps5.jpg",
+      link:`/buyPage?category=gamingConsoles&&${encryptSale50.toString()}&&id=11`
+    }
+  ];
   
   const slides = document.getElementsByClassName("slides");
   let count=0;
@@ -46,48 +64,18 @@ export default function SliderPhone() {
   return (
     <div style={{backgroundColor:'#d3d3d34a'}}>
       <div id='mainSlider'>
-          <img src="images/slider/asus.webp"
-            alt="error" 
-            className='slides'
-            onClick={()=>window.location.href=`/buyPage?category=laptops&&${encryptSale50.toString()}&&id=16`}
-            style={{cursor:"pointer"}}
-            />
-            <img src="images/slider/womenSale.webp"
-            alt="error" 
-            className='slides'
-            onClick={()=>window.location.href=`/category/women`}
-            style={{cursor:"pointer"}}
-            />
-            <img src="images/slider/menSale.webp"
-            alt="error" 
-            className='slides'
-            onClick={()=>window.location.href=`/category/men`}
-            style={{cursor:"pointer"}}
-            />
-            <img src="images/slider/mobileSale.webp"
-            alt="error" 
-            className='slides'
-            onClick={()=>window.location.href=`buyPage?category=realme&&${encryptSale40.toString()}&&id=6`}
-            style={{cursor:"pointer"}}
-            />
-            <img src="images/slider/electronicSale.webp"
-            alt="error" 
-            className='slides'
-            onClick={()=>window.location.href='/category/electronics'}
-            style={{cursor:"pointer"}}
-            />
-            <img src="images/slider/ps5.webp"
-            alt="error" 
-            className='slides'
-            onClick={()=>window.location.href=`/buyPage?category=gamingConsoles&&${encryptSale50.toString()}&&id=11`}
-            style={{cursor:"pointer"}}
-            />
-            <img src="images/slider/laptopSale.webp"
-            alt="error" 
-            className='slides'
-            onClick={()=>window.location.href=`/category/electronics/laptops`}
-            style={{cursor:"pointer"}}
-            />
+          {imgArr.map((elem,index)=>(
+            <>
+              <img src={elem.img}
+                alt="error" 
+                key={index} 
+                className='slides'
+                onClick={()=>window.location.href=elem.link}
+                style={{cursor:"pointer"}}
+                />
+            </>
+          ))
+          }
           <div id='sliderBtn'>
             <button onClick={prev} className='btn1SP' id='btnRef' >&lt;</button>
             <button onClick={next} className='btn2SP' id='btnRef' >&gt;</button>
