2bd33a23924481db9173664cbc7ad44ad16a71ee
Tushar1082 <134959329+Tushar1082@users.noreply.github.com>
2025-02-20 08:56:48 +0530

    Update slider.jsx

--- E-commerce/FrontEnd/src/components/homepage/slider/slider.jsx
+++ E-commerce/FrontEnd/src/components/homepage/slider/slider.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'; 
+import React,{useEffect} from 'react';
 import CryptoJS from 'crypto-js';
 import "./slider.css";
 
@@ -8,31 +8,31 @@ export default function SliderPhone() {
 
   const imgArr = [
     {
-      img:"images/slider/asus.jpg",
+      img:"https://firebasestorage.googleapis.com/v0/b/ecommercewebapp-40db9.appspot.com/o/MerchandiseBingoo%2FsiteOwnData%2Fasus.jpg?alt=media&token=75265768-5dee-44e1-b2da-0e3c8724755f",
       link:`/buyPage?category=laptops&&${encryptSale50.toString()}&&id=16`
     },
     {
-     img:"images/slider/womenSale.png",
+     img:"https://firebasestorage.googleapis.com/v0/b/ecommercewebapp-40db9.appspot.com/o/MerchandiseBingoo%2FsiteOwnData%2FwomenSale.png?alt=media&token=25d31ca7-d3f3-4fec-a2e2-2138e6f03b6c",
      link:"/category/women"
     },
     {
-     img:"images/slider/menSale.png",
+     img:"https://firebasestorage.googleapis.com/v0/b/ecommercewebapp-40db9.appspot.com/o/MerchandiseBingoo%2FsiteOwnData%2FmenSale.png?alt=media&token=d8f84f2e-eaf3-46f7-a6b9-f678329f0f33",
      link:"/category/men"
     },
     {
-      img:"images/slider/mobileSale.png",
+      img:"https://firebasestorage.googleapis.com/v0/b/ecommercewebapp-40db9.appspot.com/o/MerchandiseBingoo%2FsiteOwnData%2FmobileSale.png?alt=media&token=2f7a46f4-7ba3-48df-9a38-6b413da07ebd",
       link:`/buyPage?category=realme&&${encryptSale40.toString()}&&id=6`
     },
     {
-      img:"images/slider/electronicSale.jpg",
+      img:"https://firebasestorage.googleapis.com/v0/b/ecommercewebapp-40db9.appspot.com/o/MerchandiseBingoo%2FsiteOwnData%2FelectronicSale.jpg?alt=media&token=c5a30ed9-471b-4b7b-9f4d-fab1ef72fea6",
       link:"/category/electronics"
     },
     {
-      img:"images/slider/ps5.jpg",
+      img:"https://firebasestorage.googleapis.com/v0/b/ecommercewebapp-40db9.appspot.com/o/MerchandiseBingoo%2FsiteOwnData%2Fps5.jpg?alt=media&token=0c965c87-342c-47e3-bff4-c80045b23d17",
       link:`/buyPage?category=gamingConsoles&&${encryptSale50.toString()}&&id=11`
     },
     {
-      img:"images/slider/laptopSale.png",
+      img:"https://firebasestorage.googleapis.com/v0/b/ecommercewebapp-40db9.appspot.com/o/MerchandiseBingoo%2FsiteOwnData%2FlaptopSale.png?alt=media&token=e1d7804f-55d5-48e1-a63e-622f21a899f3",
       link:"/category/electronics/laptops"
     }];
   
@@ -84,7 +84,6 @@ export default function SliderPhone() {
                 className='slides'
                 onClick={()=>window.location.href=elem.link}
                 style={{cursor:"pointer"}}
-                loading="lazy"
                 />
             </>
           ))
@@ -95,4 +94,5 @@ export default function SliderPhone() {
           </div>
       </div>
     </div>
-    )};
+  )
+};
